c1c4a1cdc73bcb7126870817add675dd43e803cf
Milosz Sultanowski <milosz.sultanowski@gmail.com>
2023-07-27 00:32:00 +0200

    Keymaps update

--- lua/milosz/keymaps.lua
+++ lua/milosz/keymaps.lua
@@ -4,7 +4,7 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<C-f>", ":Telescope current_buffer_fuzzy_find<CR>", options)
 keymap("n", "<S-f>", ":Telescope live_grep<CR>", options)
-keymap("n", "<A-Tab>", ":NvimTreeToggle<CR>", options)
+keymap("n", "<C-b>", ":NvimTreeToggle<CR>", options)
 keymap("n", "<S-Left>", ":BufferLineCyclePrev<CR>", options)
 keymap("n", "<S-Right>", ":BufferLineCycleNext<CR>", options)
 keymap("n", "<S-q>", ":Bdelete!<CR>", options)
