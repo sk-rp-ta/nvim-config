46728591df3cebd9fa5b66bab8a32023e5684684
Milosz Sultanowski <milosz.sultanowski@gmail.com>
2023-08-02 23:48:08 +0200

    Added closing nvim instantly from last buffer even when tree is opened

--- lua/nvim/tree.lua
+++ lua/nvim/tree.lua
@@ -1,3 +1,27 @@
 vim.g.nvim_tree_respect_buf_cwd = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup{
+}
+
+-- auto close
+local function is_modified_buffer_open(buffers)
+    for _, v in pairs(buffers) do
+        if v.name:match("NvimTree_") == nil then
+            return true
+        end
+    end
+    return false
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if
+            #vim.api.nvim_list_wins() == 1
+            and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil
+            and is_modified_buffer_open(vim.fn.getbufinfo({ bufmodified = 1 })) == false
+        then
+            vim.cmd("quit")
+        end
+    end,
+})
